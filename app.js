7a4a924619e5cff0bb99f7ce17ce8aeed30a073e
EdgarC97 <ecardona742@gmail.com>
2024-04-25 21:29:55 -0500

    add final details

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ alert("Bienvenido al viaje de Hildebrando");
 let hSalary = 2500000; // Salario de Hildebrando
 const badFood = 15000; // Costo de la comida mala
 const goodFood = 23000; // Costo de la comida buena
+let debt = 0;
 
 //Case 1 Food election
-alert(
-  "Hildebrando está en el aeropuerto el Dorado, ya le pagaron su salario ($2.500.000) y decidirá que comer"
-);
+alert("Hildebrando está en el aeropuerto el Dorado, ya le pagaron su salario ($2.500.000) y decidirá que comer");
 // Función para elegir la comida
 function foodChoice() {
   while (true) {
@@ -17,6 +16,7 @@ function foodChoice() {
     switch (choice) {
       case "1":
         hSalary -= badFood;
+        debt += badFood;
         console.log(
           "Hildebrando eligió la almojábana con gaseosa. Le caerá mal porque lleva mucho en el stand. Ahora tiene $" +
             hSalary +
@@ -25,10 +25,9 @@ function foodChoice() {
         return;
       case "2":
         hSalary -= goodFood;
+        debt += goodFood;
         console.log(
-          "Hildebrando eligió el subway con gaseosa. Estará llenito y bien. Ahora tiene $" +
-            hSalary +
-            " disponibles."
+          "Hildebrando eligió el subway con gaseosa. Estará llenito y bien. Ahora tiene $" + hSalary + " disponibles."
         );
         return;
       case "3":
@@ -41,7 +40,9 @@ function foodChoice() {
 }
 
 foodChoice();
-
+alert(
+  "Hildebrando está en la sala de espera del aeropuerto pero debe ajustar su maleta para que pase las medidas de la aerolinea"
+);
 //Case 2 Suitcase measure
 let airportSuitcase = {
   alto: 55,
@@ -63,11 +64,7 @@ function adjustSuitcaseSize() {
   let anchoReductionFactor = airportSuitcase.ancho / hilderSuitcase.ancho;
 
   // Encontramos el factor de reducción más pequeño
-  let minReductionFactor = Math.min(
-    altoReductionFactor,
-    largoReductionFactor,
-    anchoReductionFactor
-  );
+  let minReductionFactor = Math.min(altoReductionFactor, largoReductionFactor, anchoReductionFactor);
 
   // Calculamos las nuevas dimensiones
   let nuevoAlto = hilderSuitcase.alto * minReductionFactor;
@@ -82,14 +79,12 @@ function adjustSuitcaseSize() {
   console.log(
     `Las nuevas dimensiones de la maleta de Hildebrando son: alto = ${nuevoAlto.toFixed(
       2
-    )}cm, largo = ${nuevoLargo.toFixed(2)}cm, ancho = ${nuevoAncho.toFixed(
-      2
-    )}cm`
+    )}cm, largo = ${nuevoLargo.toFixed(2)}cm, ancho = ${nuevoAncho.toFixed(2)}cm`
   );
 }
 
 adjustSuitcaseSize();
-
+alert("Hildebrando necesita utilizar el wifi para confirmar el hotel, 1 hora de Wifi le costará $50.000");
 //Case 3  Wifi Password
 let wifiPasswordBin = "01110010_01101001_01110111_01101001"; // Contraseña del wifi en binario
 let wifiCost = 50000; // Costo del wifi por 1 hora
@@ -107,12 +102,16 @@ let wifiPassword = binToText(wifiPasswordBin);
 
 // Deducimos el costo del wifi del salario de Hildebrando
 hSalary -= wifiCost;
+debt += wifiCost;
 
 console.log(
   `La contraseña del wifi es: ${wifiPassword}. Después de usar el wifi por 1h, Hildebrando tiene $${hSalary} disponibles.`
 );
 
 //Case 4 Vocal Translate
+alert(
+  "Hildebrando llega a Macondo pero se da cuenta que hay un idioma diferente, ahora debe pedir un taxi transformando las vocales a 'i'"
+);
 // Declaramos una función de flecha que toma una frase como argumento
 const translateToI = (phrase) => phrase.replace(/[aeou]/gi, "i");
 
@@ -122,6 +121,9 @@ let translatedPhrase = translateToI(phrase);
 console.log(`La frase traducida es: "${translatedPhrase}"`);
 
 //Case 5 Taxi Game
+alert(
+  "El taxista le está cobrando a Hildebrando $300.000 por la carrera, el negocea y le dice que jueguen piedra papel o tijera. si el pierde le paga completo, pero si empatan o gana no le paga nada"
+);
 let taxiFare = 300000;
 let gameResult;
 
@@ -139,9 +141,6 @@ const playGame = () => {
   let userGame;
 
   // Solicitamos la elección del usuario hasta que ingrese un número válido
-  alert(
-    "Hildebrando ahora se juega 300.000 con el taxista en piedra papel o tijeras"
-  );
   while (!userGame) {
     const userChoice = prompt("Elige: \n1. Piedra \n2. Papel \n3. Tijeras");
     userGame = options[userChoice];
@@ -159,10 +158,11 @@ const playGame = () => {
   } else {
     gameResult = "DERROTA PARA HILDREBANDO";
     hSalary -= taxiFare;
+    debt += taxiFare;
   }
 
   console.log(
-    `Jugaste ${userGame} y el taxista jugó ${taxiGame}. El juego termina en ${gameResult}. Ahora tienes $${hSalary} disponibles.`
+    `Jugaste ${userGame} y el taxista jugó ${taxiGame}. El juego termina en ${gameResult}. Ahora tiene $${hSalary} disponibles.`
   );
 };
 
@@ -186,33 +186,23 @@ const simulateActivities = () => {
     switch (userChoice) {
       case "1":
         // Si elige ir a la piscina
-        const poolChoice = prompt(
-          "El agua de la piscina huele raro. ¿Quieres meterte? \n1. Sí \n2. No"
-        );
+        const poolChoice = prompt("El agua de la piscina huele raro. ¿Quieres meterte? \n1. Sí \n2. No");
         if (poolChoice === "1") {
           // Si se mete, muere
           alive = false;
-          console.log(
-            "Hildebrando se metió a la piscina y se ahogó por demasiado cloro. Terminan las vacaciones."
-          );
+          console.log("Hildebrando se metió a la piscina y se ahogó por demasiado cloro. Terminan las vacaciones.");
         }
         break;
       case "2":
         // Si elige ir de caminatas
-        const walkChoice = prompt(
-          "¿Quieres ir toda la caminata? \n1. Sí \n2. No"
-        );
+        const walkChoice = prompt("¿Quieres ir toda la caminata? \n1. Sí \n2. No");
         if (walkChoice === "1") {
           // Si elige ir toda la caminata, llega a una hermosa cascada y se toma fotos
-          console.log(
-            "Hildebrando fue toda la caminata, llegó a una hermosa cascada y se tomó fotos."
-          );
+          console.log("Hildebrando fue toda la caminata, llegó a una hermosa cascada y se tomó fotos.");
         } else {
           // Si no, se devuelve solo y se pierde
           alive = false;
-          console.log(
-            "Hildebrando se devolvió solo de la caminata y se perdió. Terminan las vacaciones."
-          );
+          console.log("Hildebrando se devolvió solo de la caminata y se perdió. Terminan las vacaciones.");
         }
         break;
       case "3":
@@ -233,24 +223,16 @@ const simulateActivities = () => {
         break;
       case "4":
         // Si elige actividades dentro del hotel
-        const hotelChoice = prompt(
-          "Elige una actividad en el hotel: \n1. Bingo \n2. Bailar \n3. Casino y apuesta"
-        );
+        const hotelChoice = prompt("Elige una actividad en el hotel: \n1. Bingo \n2. Bailar \n3. Casino y apuesta");
         if (hotelChoice === "1") {
           // Si juega al bingo, se lo gana y aumenta su dinero
-          hSalary += 200000; // Aumentamos su salario en 200.000
-          console.log(
-            "Hildebrando ganó el bingo y aumentó su dinero. Ahora tiene $" +
-              hSalary +
-              " disponibles."
-          );
+          hSalary += 500000; // Aumentamos su salario en 200.000
+          console.log("Hildebrando ganó el bingo y aumentó su dinero. Ahora tiene $" + hSalary + " disponibles.");
         } else if (hotelChoice === "3") {
           // Si va al casino y apuesta, solo se queda con pasaje de regreso
           hSalary = taxiFare; // Su salario se reduce a la tarifa del taxi
           console.log(
-            "Hildebrando fue al casino y apostó. Ahora solo tiene $" +
-              hSalary +
-              " disponibles para el taxi de regreso."
+            "Hildebrando fue al casino y apostó. Ahora solo tiene $" + hSalary + " disponibles para el taxi de regreso."
           );
         } else {
           // Si elige bailar, la pasa muy bien
@@ -272,9 +254,8 @@ const simulateActivities = () => {
   console.log("----------------------------------------");
   console.log(`Días en Macondo: ${daysInMacondo}`);
   console.log(`Estado al final: ${alive ? "Regresó" : "F se murió"}`);
-  console.log(`Dinero gastado: $${2500000 - hSalary}`);
+  console.log(`Dinero gastado: $${debt}\n Dinero restante: $${hSalary} `);
 };
 
 // Simulamos las actividades de Hildebrando
 simulateActivities();
-sadcsa 
